88eea5bfb3897baf9c87336d1095c36f3ff4f1e2
creamynebula <mateus.souzaaa@gmail.com>
2022-09-05 11:02:50 -0300

    exercício de testes

--- 03_Iteration/repeat.go
+++ 03_Iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
-func Repeat(character string) string {
-	var repeated string //em Go, repeated == ""
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+func Repeat(character string, numRepeatings int) string {
+
+	var repeatCount = numRepeatings //numero de vezes que vamos repetir a letra
+	var repeated string             //em Go, repeated == ""
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 	return repeated
 }
--- 03_Iteration/repeat_test.go
+++ 03_Iteration/repeat_test.go
@@ -1,12 +1,22 @@
 package iteration
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	numRepeatings := 7
+	repeated := Repeat("a", numRepeatings)
+	expected := strings.Repeat("a", numRepeatings)
 
 	if repeated != expected {
 		t.Errorf("Vacilão. Era pra retornar %q, mas recebemos %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 13)
 	}
 }
