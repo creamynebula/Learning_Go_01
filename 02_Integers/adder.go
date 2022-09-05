fc5c3801300994cf33aff4106eb43eef80e88c3b
creamynebula <mateus.souzaaa@gmail.com>
2022-09-05 10:30:05 -0300

    começando iteration, e comentando os testes anteriores

--- 02_Integers/adder.go
+++ 02_Integers/adder.go
@@ -1,5 +1,6 @@
 package integers
 
+// Add recebe dois inteiros e retorna a soma deles (int)
 func Add(x, y int) int {
 	return x + y
 }
--- 02_Integers/adder_test.go
+++ 02_Integers/adder_test.go
@@ -1,13 +1,24 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestAdder(t *testing.T) {
+func TestAdderino(t *testing.T) { //veja q desde q o nome começa com TestAdd, ele reconhece a funcao Add
 	sum := Add(2, 2)
 	expected := 4
 
 	if sum != expected {
 		t.Errorf("Esperávamos %d, mas recebemos %d. GRRRRRRR.", expected, sum) //%d imprime int. %q imprimia string.
 	}
+
+}
+
+func ExampleAdd() {
+	//se o comentário embaixo tivesse 7 o valor, ia dar fail o teste! essa funcao executa teste automatico igual a outra!
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 
 }
