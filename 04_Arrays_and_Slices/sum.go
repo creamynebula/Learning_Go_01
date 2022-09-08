9d8b0d4c904226df1ab83251844f535a02aa5b7b
creamynebula <mateus.souzaaa@gmail.com>
2022-09-08 10:58:12 -0300

    slices. testes refactored.

--- 04_Arrays_and_Slices/sum.go
+++ 04_Arrays_and_Slices/sum.go
@@ -1,10 +1,18 @@
 package main
 
-func Sum(numbers [5]int) int {
+import "fmt"
+
+var numbers2 = [...]int{0, 0, 0, 0, 28}
+
+func Sum(numbers []int) int {
 	var result = 0
 	for _, elemento := range numbers {
 		result += elemento
 	}
 
 	return result
+}
+
+func main() {
+	fmt.Println(numbers2)
 }
