93f3d210aaa8b8b633541f446d4cf4045b755a2f
creamynebula <mateus.souzaaa@gmail.com>
2022-09-08 10:32:50 -0300

    range, fica mais suscinto.

--- 04_Arrays_and_Slices/sum.go
+++ 04_Arrays_and_Slices/sum.go
@@ -2,8 +2,8 @@ package main
 
 func Sum(numbers [5]int) int {
 	var result = 0
-	for i := 0; i < 5; i++ {
-		result += numbers[i]
+	for _, elemento := range numbers {
+		result += elemento
 	}
 
 	return result
