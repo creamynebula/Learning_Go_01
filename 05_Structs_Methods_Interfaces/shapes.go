7206c240912876240705325b9b19d68bcb56989c
creamynebula <mateus.souzaaa@gmail.com>
2022-09-14 09:59:52 -0300

    primeira interface

--- 05_Structs_Methods_Interfaces/shapes.go
+++ 05_Structs_Methods_Interfaces/shapes.go
@@ -19,6 +19,10 @@ func (circle Circle) Area() float64 {
 	return (circle.Radius * circle.Radius * math.Pi)
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
