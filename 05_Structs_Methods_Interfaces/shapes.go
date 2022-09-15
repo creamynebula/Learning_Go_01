0437681e05889aa9fd28f07cbe419454bdda6b7f
creamynebula <mateus.souzaaa@gmail.com>
2022-09-15 11:32:07 -0300

    mais comentários, código todo devidamente compreendido e compreensível

--- 05_Structs_Methods_Interfaces/shapes.go
+++ 05_Structs_Methods_Interfaces/shapes.go
@@ -2,12 +2,14 @@ package main
 
 import "math"
 
-type Rectangle struct {
+type Rectangle struct { // retângulos tem altura e largura
 	Width  float64
 	Height float64
 }
 
-func (rectangle Rectangle) Area() float64 {
+/* método que calcula a área de um retângulo
+ele age sobre (rectangle Rectangle), é chamado fazendo rectangle.Area(), retorna um float64 */
+func (rectangle Rectangle) Area() float64 { // metodo
 	return (rectangle.Width * rectangle.Height)
 }
 
@@ -21,7 +23,9 @@ func (circle Circle) Area() float64 {
 
 type Shape interface {
 	Area() float64
-}
+} //isso quer dizer que Shape é um struct que possui um método Area()
+// nós realmente implementamos esse método pra Circle e Rectangle, que são nossos
+// atuais usos de Shape
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
