a8321eab24e2b1784e1bc1a78074166fdfe0e375
creamynebula <mateus.souzaaa@gmail.com>
2022-09-15 12:11:16 -0300

    refactoring, e comentários

--- 05_Structs_Methods_Interfaces/shapes_test.go
+++ 05_Structs_Methods_Interfaces/shapes_test.go
@@ -19,17 +19,24 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct { //areaTests é um slice de structs
-		shape Shape   //que contém um shape
-		want  float64 //e a área desse shape
-	}{ //aí aqui declaramos literalmente 2 exemplos, que vamos usar nos testes
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		name    string  // o nome é pra gente usar no nome de cada teste, pra depois os testes serem mais legíveis
+		shape   Shape   //que contém um shape
+		hasArea float64 //e a área desse shape
+	}{ //aí aqui declararemos literalmente exemplos, que vamos usar nos testes
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+		/* os termos 'name', 'shape' e 'want' poderiam ter sido omitidos aqui, podia ter sido
+		{"Rectangle", Rectangle{12, 6}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{12, 6}, 36.0},
+		*/
 	}
 
 	for _, tt := range areaTests { //iterar sober areaTests
 		got := tt.shape.Area() // got == área do shape que tá ali colocado de exemplo
-		if got != tt.want {    // se nao for igual a área real do shape, calculada na mão
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasArea { // se nao for igual a área real do shape, calculada na mão
+			t.Errorf("got %g want %g", got, tt.hasArea)
 		}
 	}
 
