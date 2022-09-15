257b14f93b074c91f475e4d73ed61f831900f35b
creamynebula <mateus.souzaaa@gmail.com>
2022-09-15 11:54:36 -0300

    triangle implementado, testes passando, código comentado

--- 05_Structs_Methods_Interfaces/shapes.go
+++ 05_Structs_Methods_Interfaces/shapes.go
@@ -21,11 +21,18 @@ func (circle Circle) Area() float64 {
 	return (circle.Radius * circle.Radius * math.Pi)
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (triangle Triangle) Area() float64 {
+	return (triangle.Base * triangle.Height) / 2
+}
+
 type Shape interface {
 	Area() float64
-} //isso quer dizer que Shape é um struct que possui um método Area()
-// nós realmente implementamos esse método pra Circle e Rectangle, que são nossos
-// atuais usos de Shape
+} //isso quer dizer que Shape é um struct que possui um método Area() que retorna float64
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
