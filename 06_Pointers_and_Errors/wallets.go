95787779447fcd93fbbfb07911e13093bd26d5bb
creamynebula <mateus.souzaaa@gmail.com>
2022-09-19 10:04:18 -0300

    it works!

--- 06_Pointers_and_Errors/wallets.go
+++ 06_Pointers_and_Errors/wallets.go
@@ -1,13 +1,18 @@
 package main
 
+import "fmt"
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
