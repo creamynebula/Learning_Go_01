148c64ea8f29f1007bef73ca6ccd891ee76b014d
creamynebula <mateus.souzaaa@gmail.com>
2022-09-20 09:05:10 -0300

    refactoring, e novos testes

--- 06_Pointers_and_Errors/wallets.go
+++ 06_Pointers_and_Errors/wallets.go
@@ -1,15 +1,25 @@
 package main
 
-type Bitcoin int
+import "fmt"
+
+type Bitcoin int // então 10 bitcoins == Bitcoin(10)
 
 type Wallet struct {
 	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount Bitcoin) {
+func (w *Wallet) Deposit(amount Bitcoin) { // recebe um pointer pra wallet 'w'
 	w.balance += amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+type Stringer interface { // isso quer dizer que Stringer é algum type que possui um método String(), acho
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
