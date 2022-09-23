468d7daaf0ca1f7f85326ec7f0490a438a8aa749
creamynebula <mateus.souzaaa@gmail.com>
2022-09-23 09:30:26 -0300

    trabalhando no exercício de slices + alocacao dinamica

---
+++ Exercises/exercise_slices.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+
+	s1 := make([][]uint8, dy)
+
+	for y := 0; y < dy; y++ {
+		s1[y][0] = make([]uint8, dx)
+
+		for x := 0; x < dx; x++ {
+			s1[y][x] = uint8((x + y) / 2)
+		}
+	} // fim Pic
+
+	return s1
+}
+
+func main() {
+	pic.Show(Pic)
+}
