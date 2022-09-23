67ae7e327481ca5fbf99848ef62f57cec7a7f18b
creamynebula <mateus.souzaaa@gmail.com>
2022-09-23 11:07:41 -0300

    mapping!

--- Exercises/exercise_slices.go
+++ Exercises/exercise_slices.go
@@ -9,7 +9,7 @@ func Pic(dx, dy int) [][]uint8 {
 	s1 := make([][]uint8, dy)
 
 	for y := 0; y < dy; y++ {
-		s1[y][0] = make([]uint8, dx)
+		s1[y] = make([]uint8, dx)
 
 		for x := 0; x < dx; x++ {
 			s1[y][x] = uint8((x + y) / 2)
