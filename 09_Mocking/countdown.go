2c1e59a2681a6e7173526db7f524b77dcfaa547e
creamynebula <mateus.souzaaa@gmail.com>
2022-10-06 09:40:36 -0300

    mocking é confuso

--- 09_Mocking/countdown.go
+++ 09_Mocking/countdown.go
@@ -4,12 +4,40 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+const countdownStart = 3
+
+// qualquer tipo que implemente Sleep() é um Sleeper.
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() { // então, SpySleeper é um Sleep
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() { // defaultSleeper tb é um Sleep
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, "Go!")
+
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- 09_Mocking/countdown_test.go
+++ 09_Mocking/countdown_test.go
@@ -6,9 +6,12 @@ import (
 )
 
 func TestCountdown(t *testing.T) {
+	// Buffer é um slice de bytes, que vc pode ler e escrever
+	// buffer = &bytes.Buffer pq daí vc vai mexer nesse slice
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{} // sleeper com Calls == 0
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -18,5 +21,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper. want 3, got %d", spySleeper.Calls)
 	}
 }
