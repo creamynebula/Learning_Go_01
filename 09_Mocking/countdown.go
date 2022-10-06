0da4279e50537316fdd2d1b7c3489c9e9c6d6201
creamynebula <mateus.souzaaa@gmail.com>
2022-10-06 10:49:26 -0300

    countdown funcionando, com mocking nos testes, pros testes nao demorarem.

--- 09_Mocking/countdown.go
+++ 09_Mocking/countdown.go
@@ -14,6 +14,7 @@ type Sleeper interface {
 	Sleep()
 }
 
+/*
 type SpySleeper struct {
 	Calls int
 }
@@ -21,12 +22,31 @@ type SpySleeper struct {
 func (s *SpySleeper) Sleep() { // então, SpySleeper é um Sleep
 	s.Calls++
 }
+*/
 
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() { // defaultSleeper tb é um Sleep
 	time.Sleep(1 * time.Second)
 }
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+// Sleeper! No teste a gente chama esse, pq não tem os intervalos de 1s
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+// escreve "write" no slice de calls ao spy
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
