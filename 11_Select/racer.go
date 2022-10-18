4c37cf4d52ae887de203ae407cda3197a154db40
creamynebula <mateus.souzaaa@gmail.com>
2022-10-18 12:18:58 -0300

    updates

--- 11_Select/racer.go
+++ 11_Select/racer.go
@@ -6,28 +6,34 @@ import (
 	"time"
 )
 
+// por default, é pros requests darem timeout se nao tiver resposta em 10s
 var tenSecondTimeout = 10 * time.Second
 
 func Racer(a, b string) (winner string, error error) {
 	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
+// pra gente poder testar racer com um timeout menor que o default
 func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 	select {
-	case <-ping(a):
+	case <-ping(a): // se conseguir abrir a url 'a', retorna ela
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(timeout):
+	case <-time.After(timeout): // se demorar mais que timeout
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
 func ping(url string) chan struct{} {
+	// função retorna um channel struct vazio, um pointer,
+	// pq isso não ocupa memória. a idéia é retornar algo
+	// que nao gasta recurso assim que tiver aberto a url,
+	// com o objetivo de sinalizar que deu bom abrir a url apenas.
 	ch := make(chan struct{})
 	go func() {
-		http.Get(url)
-		close(ch)
+		http.Get(url) // isso blocka
+		close(ch)     // isso tb blocka
 	}()
-	return ch
+	return ch // isso blocka, pq depende de close(ch), então só retorna se tiver aberto url
 }
--- 11_Select/racer_test.go
+++ 11_Select/racer_test.go
@@ -10,16 +10,19 @@ import (
 func TestRacer(t *testing.T) {
 
 	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		// o argumento de makeDelayedServer é o delay pro tempo de resposta
 		slowServer := makeDelayedServer(20 * time.Millisecond)
 		fastServer := makeDelayedServer(0 * time.Millisecond)
-
+		// defer significa que eles vão ser executados
+		// no fim da funcao que os contém (TestRacer)
 		defer slowServer.Close()
 		defer fastServer.Close()
 
 		slowURL := slowServer.URL
 		fastURL := fastServer.URL
-
+		// Racer(url1, url2) retorna a url do site que abrir 1o
 		want := fastURL
+		// só retorna erro se der 'timeout', default == 10s
 		got, err := Racer(slowURL, fastURL)
 
 		if err != nil {
@@ -32,10 +35,11 @@ func TestRacer(t *testing.T) {
 	})
 
 	t.Run("returns an error if a server doesn't respond within the specified time", func(t *testing.T) {
+		// server com delay de 25ms
 		server := makeDelayedServer(25 * time.Millisecond)
-
 		defer server.Close()
 
+		// só que o timeout do nosso racer é 20ms, então tem que ter erro
 		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
@@ -46,6 +50,7 @@ func TestRacer(t *testing.T) {
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// nosso server dorme 'delay', e dps dá bom
 		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
